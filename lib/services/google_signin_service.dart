65a2a22023ed58b3aab828928be4f899ad3b2398
JaviKo500 <javikogutierrez64@gmail.com>
2022-04-25 19:40:07 -0500

    End section 27 google signIn with backend

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,5 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'package:http/http.dart' as http;
 class GoogleSignInService {
   
   static final GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -12,11 +12,24 @@ class GoogleSignInService {
     try {
       final account = await _googleSignIn.signIn();
       final googleKey = await account?.authentication;
-      print(' =======> id Token <==========');
+      // print(' =======> id Token <==========');
 
-      print(account);
-      print(googleKey?.idToken);
+      // print(account);
+      // print(googleKey?.idToken);
       // TODO: Call rest service backend with id token
+      final signInWithGoogleEndpoint = Uri(
+        scheme: 'https',
+        host: 'flutter-google-sign-in-jk.herokuapp.com',
+        path: '/google'
+      );
+      final session = await http.post( 
+        signInWithGoogleEndpoint,
+        body: {
+          'token': googleKey?.idToken
+        }
+      );
+      print('==========> backend');
+      print(session.body);
       return account;
     } catch (e) {
       print('Error google signin');
